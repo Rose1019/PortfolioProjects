76e1c264e249e0a36e96c2e41ba3bb6ed003a56b
Rose1019 <56750612+Rose1019@users.noreply.github.com>
2023-06-16 17:13:48 +0530

    Add files via upload

---
+++ Data_Visualization_Tableau.sql
@@ -0,0 +1,83 @@
+/* GUIDED PROJECT VISUALIZATION - ALEX THE ANALYST
+   
+   DATA SET - COVID-19 
+   
+   Website: https://ourworldindata.org/coronavirus 
+
+
+1> At frst, we have placed the population column after the date column in the excel sheet 
+	and then we have splitted the data set in to 2
+      - CovidDeaths
+      - CovidVaccination
+2> We have converted the dataset CSV format to .xlsx format whihc would help to connect to MSsql
+3> Using the option 'SQL server 2019 import and export' option we can import the data from excel to sql.
+*/
+
+***********************************************************************************************************************************
+
+---- 1> To get the GLOBAL NUMBERS on death cases comparing the new cases with new deaths
+----    Overall across the world we are looking at the  death percentage of 0.9% ~ 1%
+----    Total new cases:768272973
+----    Total_new deaths :6959019
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.Percent Population Infected Per country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+---- 4. To get the highest ibfectde count based on countries ----
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+---- 5
+
+Select dea.continent, dea.location, dea.date, dea.population
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population
+order by 1,2,3;
+
+---- 6
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2;
+
